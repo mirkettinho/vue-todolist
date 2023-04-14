fbee31e174060d9369177e238fe16fa7149da57c
mirkettinho <mirkodifranco30@gmail.com>
2023-04-14 17:35:47 +0200

    [add] bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const {createApp} = Vue;
+
+createApp({
+  data(){
+    return{
+      tasks:[
+          {
+            text: "prova 1",
+            done: false
+          },
+          {
+            text: "prova 2",
+            done: true
+          },
+          {
+            text: "prova 3",
+            done: true
+          },
+          {
+            text: "prova 4",
+            done: true
+          },
+          {
+            text: "prova 5",
+            done: true
+          }
+      ]
+    }
+  },
+  methods:{
+
+  }
+}).mount("#app")
